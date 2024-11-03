0bbcb8ed352cdf5200f71ae94406d549132e35ad
Nick-Major <divanchik26@gmail.com>
2024-11-03 15:35:50 +0300

    Added jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.jest, ...globals.node} }},
   pluginJs.configs.recommended,
 ];
